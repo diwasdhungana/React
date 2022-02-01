d6f65248e6e155b34d2b1a95de7bf640d3abb490
Asmin-Dhakal <asmindhakal11@gmail.com>
2022-02-02 00:37:11 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navBar">       
+          <h1 id="Cmart">Crypto Mart</h1>            
+      </div>
+
+      <div className='popular'>
+        <div className='pop1'>
+          
+          <div className="pop_img"><img id='gun' src='./img/glock.jpg'></img></div>  
+          <div className="popcontent">
+            <h6>Glock G-19</h6><br />
+            <p className='Subt'>This is Glock G_19 limited addition. It can help u get rid from your annoying neighbours.</p>
+            
+            </div>     
+        </div>
+        <div className='pop1'>       
+        </div>
+        <div className='pop1'>       
+        </div>
+        <div className='pop1'>       
+        </div>
+
+      </div>
+          
     </div>
   );
 }
